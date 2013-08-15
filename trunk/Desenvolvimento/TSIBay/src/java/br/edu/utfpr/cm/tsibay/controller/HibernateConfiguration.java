8da1ae0de7e377f36addb9851ffba7d58158c808
Junior <junior@junior>
2013-08-14 22:04:43 -0300

    Enviando Projeto do Netbeans

---
+++ trunk/Desenvolvimento/TSIBay/src/java/br/edu/utfpr/cm/tsibay/controller/HibernateConfiguration.java
@@ -0,0 +1,81 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.utfpr.cm.tsibay.controller;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import org.hibernate.*;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.mapping.PersistentClass;
+import org.hibernate.tool.hbm2ddl.SchemaExport;
+
+/**
+ *
+ * @author junior
+ */
+public class HibernateConfiguration {
+    // configuracões somente uma vez 
+    // build  valida somente uma vez
+    // Session uma sessão para cada transação ou um conjunto de transações
+
+    private static AnnotationConfiguration cfg = null;
+    private static SessionFactory factory = null;
+
+    public static SessionFactory getSessionFactory() {
+        if (cfg == null) {
+            cfg = new AnnotationConfiguration();
+            cfg.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
+            cfg.setProperty("hibernate.connection.username", "root");
+            cfg.setProperty("hibernate.connection.password", "123456");
+            cfg.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/samansys");
+            cfg.setProperty("hibernate.show_sql", "true");
+            cfg.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5InnoDBDialect");
+            cfg.setProperty("hibernate.connection.autocommit", "true");
+
+            factory = cfg.buildSessionFactory();// construindo uma fabrica de sessão
+
+        }
+
+        return factory;
+    }
+    //podemos setar o autocomit como true e nao precisa usar begin tansaction
+
+    public static void createSchema() {
+        getSessionFactory().close();
+        org.hibernate.tool.hbm2ddl.SchemaExport schemaEx = new SchemaExport(cfg);
+        schemaEx.create(true, true);
+    }
+
+    public static List<Object> runHQLQuery(String hql) {
+        Session session = TransactionManager.getCurrentSession();
+        return session.createQuery(hql).list();
+    }
+
+    public static Connection getConnection() {
+        Session session = TransactionManager.getCurrentSession();
+        return session.connection();
+    }
+
+    public static Query rHQLQuery(String hql) {
+        Session session = TransactionManager.getCurrentSession();
+        return session.createQuery(hql);
+    }
+
+    public static ArrayList<Class> getEntityClasses() {
+        ArrayList<Class> classes = new ArrayList<Class>();
+
+        if (cfg == null) {
+            getSessionFactory();
+        }
+
+        Iterator i = cfg.getClassMappings();
+        while (i.hasNext()) {
+            Class mappedClass = ((PersistentClass) i.next()).getMappedClass();
+        }
+        return classes;
+    }
+}
---
+++ trunk/Desenvolvimento/TSIBay/src/java/br/edu/utfpr/cm/tsibay/controller/PageManager.java
@@ -0,0 +1,84 @@
+package br.edu.utfpr.cm.tsibay.controller;
+
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name = "pageManager")
+@SessionScoped
+public class PageManager implements Serializable {
+
+    public PageManager() {
+    }
+    private String paginaAtiva = "./pages/home.xhtml";
+
+    public String getPaginaAtiva() {
+        return paginaAtiva;
+    }
+
+    public void setPaginaAtiva(String paginaAtiva) {
+        this.paginaAtiva = paginaAtiva;
+    }
+
+    public void setPg1() {
+        this.paginaAtiva = "./pages/cad_salas.xhtml";
+    }
+
+    public void setPagListaSalasReserva() {
+        this.paginaAtiva = "./pages/lista_salas_reserva.xhtml";
+    }
+
+    public void setPagListaSalas() {
+        this.paginaAtiva = "./pages/lista_salas.xhtml";
+    }
+    
+    public void setPagListaDisciplinas() {
+        this.paginaAtiva = "./pages/lista_disciplinas.xhtml";
+    }
+
+    public void setPagListaProfs() {
+        this.paginaAtiva = "./pages/lista_professores.xhtml";
+    }
+
+    public void setListaReservas() {
+        this.paginaAtiva = "./pages/lista_reservas.xhtml";
+    }
+    public void setPagCadTurmas() {
+        this.paginaAtiva = "./pages/cad_turmas.xhtml";
+    }
+    
+    public void setPagListaTurmas() {
+        this.paginaAtiva = "./pages/list_turmas.xhtml";
+    }
+
+    public void setPagListaReservasUsuario(){
+        this.paginaAtiva = "./pages/lista_reservas_usuario.xhtml";
+    }
+    public void setPg4() {
+        this.paginaAtiva = "./pages/add_professores.xhtml";
+    }
+
+    public void setPg5() {
+        this.paginaAtiva = "./pages/cad_disciplinas.xhtml";
+    }
+
+    public String getPg1() {
+        return this.paginaAtiva;
+    }
+
+    public String getPg2() {
+        return this.paginaAtiva;
+    }
+
+    public String getPg3() {
+        return this.paginaAtiva;
+    }
+
+    public String getPg4() {
+        return this.paginaAtiva;
+    }
+
+    public String getPg5() {
+        return this.paginaAtiva;
+    }
+}
