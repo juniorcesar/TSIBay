b220edaa01a34c46dff3f466dfb6a14e1b260a73
Junior <junior@junior>
2013-08-14 22:11:34 -0300

    Criando classes

---
+++ trunk/Desenvolvimento/TSIBay/src/java/br/edu/utfpr/cm/tsibay/model/Telefone.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.utfpr.cm.tsibay.model;
+
+/**
+ *
+ * @author junior
+ */
+public class Telefone {
+    
+}
