8a6a8ec09e170b8abdec86be74a18341159ff693
junior <junior@junior>
2013-08-21 20:38:48 -0300

    Criando as classes do modelo

--- trunk/Desenvolvimento/TSIBay/src/java/br/edu/utfpr/cm/tsibay/model/Endereco.java
+++ trunk/Desenvolvimento/TSIBay/src/java/br/edu/utfpr/cm/tsibay/model/Endereco.java
@@ -4,10 +4,28 @@
  */
 package br.edu.utfpr.cm.tsibay.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  *
  * @author junior
  */
+@Entity
+@Table(name = "ENDERECO")
 public class Endereco {
-    
+    @Id
+    private Long id;
+    private String bairro;
+    private String cep;
+    private String complemento;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
