88c255fe5acdd018f364287e7899058d0678895e
junior <junior@junior>
2013-09-24 17:57:00 -0300

    Alterando DaoPessoa

--- trunk/Desenvolvimento/TSIBay/src/java/br/edu/utfpr/cm/tsibay/daos/DaoPessoa.java
+++ trunk/Desenvolvimento/TSIBay/src/java/br/edu/utfpr/cm/tsibay/daos/DaoPessoa.java
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -44,38 +42,3 @@ public class DaoPessoa extends DaoGenerics<Pessoa> {
         return null;
     }
 }
-
-=======
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package br.edu.utfpr.cm.tsibay.daos;
-
-import br.edu.utfpr.cm.tsibay.model.Pessoa;
-import java.util.List;
-
-/**
- *
- * @author Alex
- */
-public class DaoPessoa extends DaoGenerics<Pessoa> {
-
-    public DaoPessoa() {
-        super(Pessoa.class);
-    }
-
-    public Pessoa getLogin(String login, String senha) {
-
-        List<Pessoa> pessoas = this.listar();
-
-        for (Pessoa p : pessoas) {
-            if (p.getLogin().equals(login) && p.getSenha().equals(senha)) {
-                return p;
-            }
-        }
-
-        return null;
-    }
-}
->>>>>>> refs/remotes/origin/master
